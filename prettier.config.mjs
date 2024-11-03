c4aeec117127ce5bd07ebd4dfa875169a54cba96
Niklas Lindroos <niklas.lindroos@alumni.aalto.fi>
2024-11-03 10:20:41 +0200

    create custom prettier config and use exact dependency versions

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Options} */
+export default {
+  arrowParens: 'avoid',
+  printWidth: 80,
+  proseWrap: 'always',
+  semi: false,
+  singleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
